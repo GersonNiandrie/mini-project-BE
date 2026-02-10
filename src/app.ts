071fc94fc3036ce5d1455d4efd596c5b6007ba26
fachrigans <fachri@gmail.com>
2026-02-10 17:47:38 +0700

    creat centralized and error handling

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,20 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 const PORT: number = 8000;
 
 const app = express();
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.log(err);
+  const statusCode = err.expose === true ? err.statusCode : 500;
+  const message = err.expose === true ? err.message : "Something went wrong";
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    data: null,
+  });
+});
 
 app.listen(PORT, () => {
     console.log(`Application running on port ${PORT}`);
