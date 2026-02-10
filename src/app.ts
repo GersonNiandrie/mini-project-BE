17f399fbf9d3f9ffa28fa609111f22d97afb075c
fachrigans <fachri@gmail.com>
2026-02-10 21:14:01 +0700

    create global error handling

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import eventsRouter from "./router/events.router";
 
 const PORT: number = 8000;
@@ -6,6 +6,17 @@ const PORT: number = 8000;
 const app = express();
 
 app.use('/api/events', eventsRouter)
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  const statusCode = err.expose === true ? err.statusCode : 500;
+  const message = err.expose === true ? err.message : "Semothing went wrong";
+
+  res.status(statusCode).json({
+    success: false,
+    message: message,
+    data: null,
+  });
+});
 
 app.listen(PORT, () => {
     console.log(`Application running on port ${PORT}`);
