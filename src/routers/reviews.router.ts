d3f4e91164067e68ba51bd31c8e2fb51f9040ca6
fachrigans <fachri@gmail.com>
2026-02-23 01:37:19 +0700

    add validator on create review

--- src/routers/reviews.router.ts
+++ src/routers/reviews.router.ts
@@ -3,11 +3,13 @@ import { reviewsController } from "../controllers/reviews.controller";
 import { jwtVerify } from "../middlewares/auth.middleware";
 import { JWT_TOKEN_SECRET_KEY } from "../config/main.config";
 import { customerOnly } from "../middlewares/customerOnly.middleware";
+import { createReviewValidator } from "../../validators/review.validator";
+import { expressRequestValidation } from "../middlewares/express-request-validation.middleware";
 
 const reviewsRouter = Router();
 
 reviewsRouter.get("/:eventId",jwtVerify(JWT_TOKEN_SECRET_KEY!), customerOnly, reviewsController.get);
-reviewsRouter.post("/", jwtVerify(JWT_TOKEN_SECRET_KEY!), customerOnly, reviewsController.create);
+reviewsRouter.post("/", jwtVerify(JWT_TOKEN_SECRET_KEY!), customerOnly, createReviewValidator, expressRequestValidation, reviewsController.create);
 reviewsRouter.delete("/:id", reviewsController.delete);
 
 export default reviewsRouter;
