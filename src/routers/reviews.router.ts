dff2c53722934738d811c5d45d1ebc0ac28d22dd
fachrigans <fachri@gmail.com>
2026-02-23 01:30:33 +0700

    add role guard

--- src/routers/reviews.router.ts
+++ src/routers/reviews.router.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { reviewsController } from "../controllers/reviews.controller";
+import { jwtVerify } from "../middlewares/auth.middleware";
+import { JWT_TOKEN_SECRET_KEY } from "../config/main.config";
+import { customerOnly } from "../middlewares/customerOnly.middleware";
 
 const reviewsRouter = Router();
 
-reviewsRouter.get("/:eventId", reviewsController.get);
-reviewsRouter.post("/", reviewsController.create);
+reviewsRouter.get("/:eventId",jwtVerify(JWT_TOKEN_SECRET_KEY!), customerOnly, reviewsController.get);
+reviewsRouter.post("/", jwtVerify(JWT_TOKEN_SECRET_KEY!), customerOnly, reviewsController.create);
 reviewsRouter.delete("/:id", reviewsController.delete);
 
 export default reviewsRouter;
